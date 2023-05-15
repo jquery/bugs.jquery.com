fa6d37d26ffc82c1382cb50b4ff7c8635b0b6ebe
Timmy Willison <timmywil@users.noreply.github.com>
2023-05-14 21:17:29 -0400

    First commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "jQuery - Bug Tracker",
+	url: "https://bugs.jquery.com/",
+	language: "en",
+	description: "Static archive of the old bugs.jquery.com trac site.",
+	author: {
+		name: "Timmy Willison",
+		email: "timmywil@users.noreply.github.com",
+		url: "https://timmywil.com/",
+	},
+};
